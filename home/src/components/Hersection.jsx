c51a05332b2894e573215e2534f0485d1853b54a
Gamsayini.Somasekaram <gamsagamsi@gmail.com>
2025-07-25 07:00:47 +0530

    my mor

---
+++ home/src/components/Hersection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+import { Typewriter } from 'react-simple-typewriter';
+
+function Hersection() {
+  return (
+<>
+
+</>
+  )
+}
+
+export default Hersection
--- home/src/main.jsx
+++ home/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Hersection from './components/Hersection'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <Hersection />
   </StrictMode>,
 )
